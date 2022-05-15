70f402c840fa4b045dc8d2e0af4dba488bd8ee12
leech <fairprice81000@gmail.com>
2022-05-15 20:24:50 +0800

    fix url

--- src/api/fetchAqiData.jsx
+++ src/api/fetchAqiData.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { AqiKey } from "./ApiKey";
-const URL = "http://api.waqi.info/feed/geo";
+const URL = "https://api.waqi.info/feed/geo";
 const api = AqiKey;
 const FetchAqiData = async (latitude, longitude) => {
   const { data } = await axios.get(
